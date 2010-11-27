726ca792d0d20f724b5c8709f6352c0e221778aa
Guillermo <guilleiguaran@gmail.com>
2010-11-26 23:43:35 -0500

    Chapter 1 examples

---
+++ chp01/echoargs.scala
@@ -0,0 +1,9 @@
+var i = 0
+while(i < args.length)
+{
+	if(i != 0)
+		print(" ")
+	print(args(i))
+	i += 1
+}
+print("\n")
---
+++ chp01/printargs.scala
@@ -0,0 +1,7 @@
+var i = 0
+println("I'll print " + args.length + " args")
+while(i < args.length)
+{
+	println(args(i))
+	i +=1
+}
