5e2cf865aefa9b424c26e2ad18bdc8edec8d1fd7
Guillermo <guilleiguaran@gmail.com>
2010-12-24 01:26:52 -0500

    two more specs for Rational class

--- programming_scala_book/chp14/RationalSpecification.scala
+++ programming_scala_book/chp14/RationalSpecification.scala
@@ -2,13 +2,24 @@ import org.specs._
 
 object RationalSpecification extends Specification {
   "A Rational" should {
+	val r1 = new Rational(1, 2)
+	val r2 = new Rational(1, 2)
 	"have a numerator equal to the passed value" in {
-		val number = new Rational(1, 2)
-		number.numer must be_==(1)
+		r1.numer must be_==(1)
 	}
-	"have a denominator requal to the passed value" in {
-		val number = new Rational(1, 2)
-		number.denom must be_==(2)
+	"have a denominator equal to the passed value" in {
+		r1.denom must be_==(2)
+	}
+	"have a formated representation in n/d format" in {
+		r1.toString must be_==("1/2")
+	}
+	"be summed with other rational number" in {
+		val r3 = r1 + r2
+		r3.toString must be_==("4/4")
+	}
+	"be substracted with other rational number" in {
+		val r3 = r1 - r2
+		r3.toString must be_==("0/4")
 	}
   }
 }
