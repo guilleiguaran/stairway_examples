721a7ec2179cbe6e4d0a8931dfb7909ad855e985
guilleiguaran <guilleiguaran@gmail.com>
2011-01-30 00:37:53 -0500

    (testing Scala.vim)

--- chp19/Queue.scala
+++ chp19/Queue.scala
@@ -1,28 +1,61 @@
+/* Using private class
+
 trait Queue[T]{
-	def head: T
-	def tail: Queue[T]
-	def append(x: T): Queue[T]
+  def head: T
+  def tail: Queue[T]
+  def append(x: T): Queue[T]
 }
 
 object Queue {
-	
-	def apply[T](xs: T*): Queue[T] = new QueueImpl[T](xs.toList, Nil)
-	
-	private class QueueImpl[T](
-		private val leading: List[T], 
-		private val trailing: List[T]
-	) extends Queue[T] {
-		
-	  def mirror =
-			if(leading.isEmpty) new QueueImpl(trailing.reverse, Nil)
-			else this
-		
-		def head: T = mirror.leading.head
-		
-		def tail: QueueImpl[T] = {
-			val q = mirror
-			new QueueImpl(q.leading.tail, q.trailing)
-		}
-		def append(x: T) = new QueueImpl(leading, x :: trailing)
-	}
+
+  def apply[T](xs: T*): Queue[T] = new QueueImpl[T](xs.toList, Nil)
+
+  private class QueueImpl[T](
+    private val leading: List[T],
+    private val trailing: List[T]
+  ) extends Queue[T] {
+
+    private def mirror =
+      if(leading.isEmpty) new QueueImpl(trailing.reverse, Nil)
+      else this
+
+    def head: T = mirror.leading.head
+
+    def tail: QueueImpl[T] = {
+      val q = mirror
+      new QueueImpl(q.leading.tail, q.trailing)
+    }
+
+    def append(x: T) = new QueueImpl(leading, x :: trailing)
+  }
+}*/
+
+/* Using private constructor */
+
+class Queue[T] (private val leading: List[T], private val trailing: List[T]) {
+
+  private def mirror =
+    if(leading.isEmpty) new Queue(trailing.reverse, Nil)
+    else this
+
+  def head: T = mirror.leading.head
+
+  def tail: Queue[T] = {
+    val q = mirror
+    new Queue(q.leading.tail, q.trailing)
+  }
+
+  def append(x: T) = new Queue(leading, x :: trailing)
+
+  override def toString: String = {
+    val queue = leading ::: trailing.reverse
+    val listString =
+      if(queue.isEmpty) ""
+      else queue.mkString(", ")
+      "Queue(" + listString + ")"
+  }
+}
+
+object Queue {
+  def apply[T](xs: T*): Queue[T] = new Queue[T](xs.toList, Nil)
 }
